78e52e2e60ea51eb2777e8b343c96692c1873685
elsammio <109492335+elsammio@users.noreply.github.com>
2024-08-06 17:09:28 -0500

    Modificación del main

--- conjuntos/src/App.java
+++ conjuntos/src/App.java
@@ -1,8 +1,8 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         /*  Crear un programa que me permita realizar la unión y la intersección de dos conjuntos y que a futuro podamos agregarle más operaciones */
 
     System.out.println("Hola mundo");
-    
+
     }
 }
