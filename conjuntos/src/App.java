94cdf7b271f7a1f2d5c11429f46c203fe3ae128a
Carlos Samir Diaz <elsammio@gmail.com>
2024-08-06 15:33:09 -0500

    Creación del proyecto conjuntos

---
+++ conjuntos/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        /*  Crear un programa que me permita realizar la unión y la intersección de dos conjuntos y que a futuro podamos agregarle más operaciones */
+
+    System.out.println("Hola mundo");
+    
+    }
+}
