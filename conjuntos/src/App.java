3d676720e404d2bf75edcc1549340f537a5c6e41
elsammio <109492335+elsammio@users.noreply.github.com>
2024-08-09 17:22:37 -0500

    Elimine el proyecto conjuntos

--- conjuntos/src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args){
-        /*  Crear un programa que me permita realizar la unión y la intersección de dos conjuntos y que a futuro podamos agregarle más operaciones */
-        
-
-    }
-}
