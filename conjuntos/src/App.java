68d9c6b8127c12afec1190d861225a2018e0a700
elsammio <109492335+elsammio@users.noreply.github.com>
2024-08-09 08:44:18 -0500

    se borran los sout

--- conjuntos/src/App.java
+++ conjuntos/src/App.java
@@ -1,11 +1,7 @@
 public class App {
     public static void main(String[] args){
         /*  Crear un programa que me permita realizar la unión y la intersección de dos conjuntos y que a futuro podamos agregarle más operaciones */
-
-    System.out.println("Hola mundo");
-
-    System.out.println("ESTA ES UNA PRUEBA DE GIT en la nube");
-    System.out.println("ESTA ES UNA PRUEBA DE GIT en el local");
+        
 
     }
 }
