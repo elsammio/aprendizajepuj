184be4e8f32faa906298372a35fdb8e522ea36e0
elsammio <109492335+elsammio@users.noreply.github.com>
2024-08-09 18:29:23 -0500

    Creación del proyecto conjuntos

---
+++ conjuntos/poo/Persona.java
@@ -0,0 +1,17 @@
+package conjuntos.poo;
+
+public class Persona {
+    String nombres;
+    int edad;
+    String carrera;
+
+    public Persona(){
+        nombres = "Samir";
+        edad = 39;
+        carrera = "Ingenieria de sistemas";
+        
+    
+    }
+    
+}
+
