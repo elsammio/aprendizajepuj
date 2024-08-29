d01c66ac85296457504794e4e542ac00e619a1ec
elsammio <109492335+elsammio@users.noreply.github.com>
2024-08-29 18:07:17 -0500

    Actualizacion proyecto

--- conjunto/src/conjunto/Estudiante.java
+++ conjunto/src/conjunto/Estudiante.java
@@ -5,12 +5,43 @@ public class Estudiante {
 	private String nombres;
 	private String apellidos;
 	
-	public Estudiante(int idEstudiante, String nombres, String apellidos) {
+	public Estudiante(int idEstudiante, String nombres, String apellidos) {//contructor
 		super();
 		this.idEstudiante = idEstudiante;
 		this.nombres = nombres;
 		this.apellidos = apellidos;
 	}
+
+	@Override
+	public String toString() {
+		return "Estudiante [idEstudiante=" + idEstudiante + ", nombres=" + nombres + ", apellidos=" + apellidos + "]";
+	}
+
+	public int getIdEstudiante() {
+		return idEstudiante;
+	}
+
+	public void setIdEstudiante(int idEstudiante) {
+		this.idEstudiante = idEstudiante;
+	}
+
+	public String getNombres() {
+		return nombres;
+	}
+
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	
+	
 	
 	
 	
--- conjunto/src/conjunto/Main.java
+++ conjunto/src/conjunto/Main.java
@@ -1,12 +1,38 @@
 package conjunto;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		ArrayList<Estudiante> estudiante = new ArrayList<>();
+		List<Estudiante> sistemas = new ArrayList<>();
+		
+		List<Estudiante> economia = new ArrayList<>();
+		
+		//Estudiante estudiante = new Estudiante(1, "Samir", "Diaz");
+		
+		sistemas.add(new Estudiante (1,"Samir", "Diaz"));
+		sistemas.add(new Estudiante (2,"Alexander", "Filigrana"));
+		sistemas.add(new Estudiante (3,"Monica", "Perdomo"));
+		
+		economia.add(new Estudiante (4,"Alexander", "Alegria"));
+		economia.add(new Estudiante (5,"Andres", "Gutierrez"));
+		economia.add(new Estudiante (1,"Samir", "Diaz"));
+		
+		for(int i=0; i < sistemas.size(); i ++) {
+			
+			System.out.println(sistemas.get(i));
+			
+			
+		}
+		
+		
+		
+		
+		
+		
 		
 		
 	}
