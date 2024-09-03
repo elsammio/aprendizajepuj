7baebd27388f6c3a169729f2425f4d50ea46b47a
elsammio <109492335+elsammio@users.noreply.github.com>
2024-09-03 13:23:41 -0500

    Proyecto actualizado

---
+++ PrimerosPasos/src/diaz/samir/entradaSalida/EntradaScanner.java
@@ -0,0 +1,19 @@
+package diaz.samir.entradaSalida;
+
+import java.util.Scanner;
+
+public class EntradaScanner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Digita tu nombre, por favor: ");
+		
+		String nombre = entrada.nextLine();
+		
+		System.out.println("Hola " + nombre);
+	}
+
+}
