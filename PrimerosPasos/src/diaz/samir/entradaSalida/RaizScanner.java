f23a15b690f8371492e8ce56d6c0c9c4cf02167f
elsammio <109492335+elsammio@users.noreply.github.com>
2024-09-05 20:24:34 -0500

    Proyecto actualizado

--- PrimerosPasos/src/diaz/samir/entradaSalida/RaizScanner.java
+++ PrimerosPasos/src/diaz/samir/entradaSalida/RaizScanner.java
@@ -11,8 +11,9 @@ public class RaizScanner {
 		
 		//System.out.println("la raiz cuadra de " + num + " y su raiz cuadrada es " + Math.sqrt(num));
 		
+		// Crea una clase con el nombre de RaizScanner. Al ejecutar el programa nos debe pedir introducir un nº por consola.
 		
-		/*Scanner entrada = new Scanner (System.in);
+		Scanner entrada = new Scanner (System.in);
 		
 		System.out.println("Digite un numero: ");
 		
@@ -20,8 +21,11 @@ public class RaizScanner {
 		
 		double raiz = Math.sqrt(numero);
 		
-		System.out.println("La raiz de " + numero + " es " + raiz);*/
+		System.out.println("La raiz de " + numero + " es " + raiz);
 		
+		//Codigo simplificado
+		
+		//Modifica el programa del ejercicio anterior para reducir lo máximo posible el nº de líneas de código.
 		
         Scanner entrada = new Scanner (System.in);
 		
