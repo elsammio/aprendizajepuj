4c39475cd39b9f168ebf343eb4fc5914f01c4d7c
elsammio <109492335+elsammio@users.noreply.github.com>
2024-09-05 19:32:34 -0500

    Proyecto actualizado

--- PrimerosPasos/src/diaz/samir/entradaSalida/CalculoPotenciaJOptionPane.java
+++ PrimerosPasos/src/diaz/samir/entradaSalida/CalculoPotenciaJOptionPane.java
@@ -7,9 +7,25 @@ public class CalculoPotenciaJOptionPane {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//Crea una clase con el nombre de CalculoPotenciaJOptionPane. Al ejecutar el programa nos debe 
+		//pedir introducir dos valores numéricos utilizando una ventana de tipo showInputDialog
+		
 		int base = Integer.parseInt(JOptionPane.showInputDialog("Introduce la base  por favor: "));
 		
 		int exponente = Integer.parseInt(JOptionPane.showInputDialog("Introduce el exponente  por favor: "));
+		
+		double resultado = Math.pow(base, exponente);
+		
+		System.out.println("EL resultado de " + base + " elevado a " + exponente + " es " + resultado );
+		
+		//Codigo simplificado
+		
+		//Modifica el programa del ejercicio anterior para reducir lo máximo posible el nº de líneas de código. 
+		
+		double resultado2 = Math.pow(Integer.parseInt(JOptionPane.showInputDialog("Introduce la base  por favor: ")), Integer.parseInt(JOptionPane.showInputDialog("Introduce el exponente  por favor: ")));
+		
+		System.out.println("El resultado es: " + resultado2);
+		
 		
 		
 
--- PrimerosPasos/src/diaz/samir/entradaSalida/RaizScanner.java
+++ PrimerosPasos/src/diaz/samir/entradaSalida/RaizScanner.java
@@ -1,16 +1,37 @@
 package diaz.samir.entradaSalida;
 
-import javax.swing.*;
+import java.util.*;
 
 public class RaizScanner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int num = Integer.parseInt(JOptionPane.showInputDialog("Introduce un numero por favor: "));
+		//int num = Integer.parseInt(JOptionPane.showInputDialog("Introduce un numero por favor: "));
 		
-		System.out.println("la raiz cuadra de " + num + " y su raiz cuadrada es " + Math.sqrt(num));
+		//System.out.println("la raiz cuadra de " + num + " y su raiz cuadrada es " + Math.sqrt(num));
 		
+		
+		/*Scanner entrada = new Scanner (System.in);
+		
+		System.out.println("Digite un numero: ");
+		
+		int numero = entrada.nextInt();
+		
+		double raiz = Math.sqrt(numero);
+		
+		System.out.println("La raiz de " + numero + " es " + raiz);*/
+		
+		
+        Scanner entrada = new Scanner (System.in);
+		
+		System.out.println("Digite un numero: ");
+				
+		double raiz = Math.sqrt(entrada.nextInt());
+		
+		System.out.println("La raiz es " + raiz);
+		
+		entrada.close();
 
 	}
 
