5be8ab43a1ce9c236438ed3d02d57d3eea3e6029
elsammio <109492335+elsammio@users.noreply.github.com>
2024-09-13 18:22:12 -0500

    Proyecto actualizado

--- conjunto/src/conjunto/Main.java
+++ conjunto/src/conjunto/Main.java
@@ -25,7 +25,38 @@ public class Main {
 		economia.add(new Estudiante (1,"Samir", "Diaz"));
 		economia.add(new Estudiante (2,"Alexander", "Filigrana"));
 		
+		/*for(int i=0; i < sistemas.size(); i ++) {
 		
+		for(int j = 0; j< economia.size(); j ++) {
+			
+			if(sistemas.get(i).getIdEstudiante()== economia.get(j).getIdEstudiante()) {
+				
+				interseccion.add(new Estudiante (sistemas.get(i).getIdEstudiante(),sistemas.get(i).getNombres(), sistemas.get(i).getApellidos()));
+				
+				System.out.println(interseccion);
+				
+			}
+			
+		}			
+		
+	}*/
+		
+		
+		for(Estudiante estudianteSistemas : sistemas){
+			for(Estudiante estudianteEconomia : economia) {
+				
+				if(estudianteSistemas.getIdEstudiante() == estudianteEconomia.getIdEstudiante()) {
+					
+					interseccion.add(new Estudiante(estudianteSistemas.getIdEstudiante(),estudianteSistemas.getNombres(),estudianteSistemas.getApellidos()));
+					
+					System.out.println(interseccion);
+					
+				}
+				
+			}
+		}
+		
+	
 		
 		
 		
