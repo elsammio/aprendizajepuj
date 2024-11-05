66d6e13492407e8d8b9a960e7b988517a09ba4c2
elsammio <109492335+elsammio@users.noreply.github.com>
2024-11-05 18:58:14 -0500

    Actualizacion proyecto

---
+++ FizzBuzz/src/fizzbuzz/Main.java
@@ -0,0 +1,59 @@
+package fizzbuzz;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * Escribe un programa que muestre por consola (con un print) los
+		 * números de 1 a 100 (ambos incluidos y con un salto de línea entre
+		 * cada impresión), sustituyendo los siguientes:
+		 * - Múltiplos de 3 por la palabra "fizz".
+		 * - Múltiplos de 5 por la palabra "buzz".
+		 * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+		 */
+		
+		int num = 11;
+		
+		int j = 3;
+		
+		int tres = 3;
+		
+		double resultado;
+		
+		resultado = j / tres;
+		
+		System.out.println(resultado);
+		
+		if(resultado != Math.floor(resultado) ) {
+			
+			System.out.println("El numero "   + j + " es multiplo de 3");
+			
+		}else {
+			
+			System.out.println("El numero "   + j + " no es multiplo de 3");
+		}
+		
+		
+		
+		for (int i = 1; i < num; i++) {
+			
+			
+			
+			//System.out.println(i);
+			
+			
+			/*if(entero == Math.floor(entero) ) {
+				
+				System.out.println("El numero " + entero + " es multiplo de 3");
+				
+			}*/
+			
+		}
+
+		
+		
+
+	}
+
+}
---
+++ Numeros100/src/numeros/Main.java
@@ -0,0 +1,30 @@
+package numeros;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num = 0;
+		
+		int num1 = 101;
+		
+		while (num < 101) {
+			
+			System.out.println(num);
+			
+			num = num + 1;
+		}
+		
+		
+		for (int i = 0; i < num1; i++) {
+			
+			System.out.println(i);
+			
+			
+		}
+		
+
+	}
+
+}
